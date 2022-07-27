99e3ab70796648dd71d181c9f653a1d20f415e38
Katie Baeten <katherinebaeten@gmail.com>
2022-07-27 16:43:16 -0500

    backend + frontend together
    Things are not working so well. Database is not connected properly.

---
+++ public/home.js
@@ -0,0 +1,14 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="Better Banking LLC"
+      title="Welcome!"
+      text="You can move around using the navigation bar."
+      
+    />
+  );  
+}
+
+
+//body={(<img src="" className="img-fluid" alt="Responsive image"/>)}
