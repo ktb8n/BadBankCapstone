fce7b94c05cccc92a226058385fc5c6d741d7ab8
Katie Baeten <katherinebaeten@gmail.com>
2022-08-01 14:33:23 -0500

    found typo and hoped it would solve everything.
    it did not.

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ function Spa() {
         <UserContext.Provider value={{users:[{name:'Harold',email:'purplecrayons@KG.edu',password:'secret',balance:100}]}}>
           <div className="container" style={{padding: "20px"}}>
             <Route path="/" exact component={Home} />
-            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/createAccount/" component={CreateAccount} />
             <Route path="/login/" component={Login} />
             <Route path="/deposit/" component={Deposit} />
             <Route path="/withdraw/" component={Withdraw} />
